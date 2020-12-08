c9f0254d716ee67aa9c396321d9b03b45c14294d
Seeyozz <73107355+Seeyozz@users.noreply.github.com>
2020-12-08 20:50:59 +0100

    Revert "huffman tree"

--- main.h
+++ main.h
@@ -1,42 +1,22 @@
-#ifndef h_main
-#define h_main
-#define _GNU_SOURCE
+#ifndef  h_main
+#define  h_main
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-
-typedef struct Node
-{
-    int occur;
-    char data;
-    struct Node *left;
-    struct Node *right;
-} Node;
 
 typedef struct List
 {
     struct List *next;
     int occur;
     char c;
-    struct Node *tree;
-    int id;
-} List;
-
-typedef struct List_Node
-{
-    struct Node* data;
-    struct List_Node *next;
-    int id;
-}List_Node;
+}List;
 
 #include "bit.h"
 #include "occurences.h"
 #include "tree.h"
 
-char *fileToString(const char *filename);
-void printList(struct List *list);
-void deleteNode(struct List_Node **head_ref, int key);
+char *fileToString(char *filename);
+void printList(struct List* list);
 
-#endif
+#endif 
--- occurences.h
+++ occurences.h
@@ -5,6 +5,5 @@
 
 void writeOccur(char* filename);
 void listOccur(struct List* result, char *filename);
-List *create_Element(char c, int id, int occur);
-List *create_list(int size);
+
 #endif
